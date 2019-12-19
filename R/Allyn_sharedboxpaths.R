415a2f364221f439ff2f0bae88ecaeb66f94c25f
Adam A. Kemberling <adamkemberling@gmail.com>
2019-12-19 11:15:07 -0500

    Andrew's boxpath functions

--- R/Allyn_sharedboxpaths.R
+++ R/Allyn_sharedboxpaths.R
@@ -3,9 +3,17 @@
 ####
 
 # Detect the operating system
-os.use<- .Platform$OS.type
+os.use <- .Platform$OS.type
 
-# Set path to shared folders (need someone to check this for "windows" option)
+#' @title Enter User Name
+#'
+#' @description Prompts user name entry for determining box location on local machine.
+#' (needs final check for "windows" machine users). Mac users do not need this step.
+#'
+#' @return user.name User name
+#' @export
+#'
+#' @examples
 enter_user_name_func<- function(){
   user.name<- readline(prompt= "Enter user name: ")
   return(user.name)
@@ -17,17 +25,25 @@ if(os.use == "windows"){
   print("MacOS user name not required")
 }
 
-shared.path<- function(os.use = os.use, group = c("RES Data", "Mills Lab"), folder = "Functions/"){
-  # Details: This function creates paths to shared Data and Functions folders stored either inside the Mills Lab folder or outside the Mills Lab folder
 
-  # Args:
-  # os.use = Character string defining operating system. Either "unix" or "windows"
-  # group = Character string defining where to load shared Data and Functions. Either RES (things shared across the entire research department) or Mills Lab (things shared within the Mills Lab)
-  # folder = Character string defining the folder type. Either "Data/" or "Functions/" Note that it must include the last forward slash!
-
-  # Returns: Path to shared folder. We will then use these paths within shared code to load shared data or source shared functions. For example:
-  # res.data.path<- shared.path(os.use = os.use, group = "RES", folder = "Data/")
-  # gom.shapefile<- st_read(paste(res.data.path, "Shapefiles/GoM_sf.shp", sep = ""))
+#' @title Shared Path
+#'
+#' @description This function creates paths to shared Data and Functions folders stored either inside the Mills Lab folder or outside the Mills Lab folder
+#'
+#' @param os.use Character string defining operating system. Either "unix" or "windows"
+#' @param group Character string defining where to load shared Data and Functions. Either RES (things shared across the entire research department) or Mills Lab (things shared within the Mills Lab)
+#' @param folder
+#'
+#' @return path.out Path to shared folder. We will then use these paths within shared code to load shared data or source shared functions.
+#' @export
+#'
+#' @examples
+#' res.data.path<- shared.path(os.use = os.use, group = "RES", folder = "Data/")
+#'
+#' # Not Run
+#' # gom.shapefile<- st_read(paste(res.data.path, "Shapefiles/GoM_sf.shp", sep = ""))
+#'
+shared.path <- function(os.use = os.use, group = c("RES Data", "Mills Lab"), folder = "Functions/"){
 
   if(os.use == "unix"){
     if(!is.null(group)){
@@ -53,10 +69,10 @@ shared.path<- function(os.use = os.use, group = c("RES Data", "Mills Lab"), fold
 
 
 
-res.data.path<- shared.path(os.use = os.use, group = "RES Data", folder = "")
-res.func.path<- shared.path(os.use = os.use, group = "RES Data", folder = "")
-lab.data.path<- shared.path(os.use = os.use, group = "Mills Lab", folder = "Data/")
-lab.func.path<- shared.path(os.use = os.use, group = "Mills Lab", folder = "Functions/")
+res.data.path <- shared.path(os.use = os.use, group = "RES Data", folder = "")
+res.func.path <- shared.path(os.use = os.use, group = "RES Data", folder = "")
+lab.data.path <- shared.path(os.use = os.use, group = "Mills Lab", folder = "Data/")
+lab.func.path <- shared.path(os.use = os.use, group = "Mills Lab", folder = "Functions/")
 proj.path<- shared.path(os.use = os.use, group = NULL, folder = NULL)
 
 cat("You did it! You have created paths to RES and Mills Lab shared Data and Functions folders and the project folder on Box.\n", paste("The paths can be called directly using res.data.path, res.func.path, lab.data.path, lab.func.path and proj.path. Go foRth and conqueR!", sep = ""))
