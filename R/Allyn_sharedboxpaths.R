14ad05bbee6210d742fb4ca1f3d1fd7fb21a58aa
Adam <adamkemberling@gmail.com>
2020-09-24 11:24:55 -0400

    changes to add NSF box data

--- R/Allyn_sharedboxpaths.R
+++ R/Allyn_sharedboxpaths.R
@@ -54,14 +54,15 @@ enter_user_name_func<- function(){
 #' # Not Run
 #' # gom.shapefile<- st_read(paste(res.data.path, "Shapefiles/GoM_sf.shp", sep = ""))
 #'
-shared.path <- function(os.use = "unix", group = c("RES Data", "Mills Lab", "Climate Change Ecology Lab"), folder = "Functions/"){
+shared.path <- function(os.use = "unix", group = c("RES Data", "Mills Lab", "Climate Change Ecology Lab", "NSF OKN Demo Data"), folder = "Functions/"){
 
   if(os.use == "unix"){
     if(!is.null(group)){
       path.out<- switch(group,
                         "RES Data" = paste("~/Box/RES Data/", sep = ""),
                         "Mills Lab" = paste("~/Box/Mills Lab/", folder, sep = ""),
-                        "Climate Change Ecology Lab" = paste("~/Box/Climate Change Ecology Lab/", folder, sep = ""))
+                        "Climate Change Ecology Lab" = paste("~/Box/Climate Change Ecology Lab/", folder, sep = ""),
+                        "NSF OKN" = paste("~/Box/NSF OKN Demo Data/", folder, sep = ""))
     } else {
       path.out<- paste("~/Box/Mills Lab/Projects/", sub('.*\\/', '', getwd()), "/", sep = "")
     }
@@ -70,7 +71,9 @@ shared.path <- function(os.use = "unix", group = c("RES Data", "Mills Lab", "Cli
       path.out<- switch(group,
                         "RES Data" = paste("C:/Users/", user.name, "/Box/Res Data/", sep = ""),
                         "Mills Lab" = paste("C:/Users/", user.name, "/Box/Mills Lab/", folder, sep = ""),
-                        "Climate Change Ecology Lab" = paste("C:/Users/", user.name, "/Box/Climate Change Ecology Lab/", folder, sep = ""))
+                        "Climate Change Ecology Lab" = paste("C:/Users/", user.name, "/Box/Climate Change Ecology Lab/", folder, sep = ""),
+                        "NSF OKN" = paste("C:/Users/", user.name, "/Box/NSF OKN Demo Data/", folder, sep = ""),
+                        )
     } else {
       path.out<- paste("C:/Users/", user.name, "/Box/Mills Lab/Projects/", sub('.*\\/', '', getwd()), "/", sep = "")
     }
@@ -83,12 +86,3 @@ shared.path <- function(os.use = "unix", group = c("RES Data", "Mills Lab", "Cli
   return(path.out)
 }
 
-
-
-# res.data.path <- shared.path(os.use = "unix", group = "RES Data", folder = "")
-# res.func.path <- shared.path(os.use = "unix", group = "RES Data", folder = "")
-# lab.data.path <- shared.path(os.use = "unix", group = "Mills Lab", folder = "Data/")
-# lab.func.path <- shared.path(os.use = "unix", group = "Mills Lab", folder = "Functions/")
-# proj.path     <- shared.path(os.use = "unix", group = NULL, folder = NULL)
-
-cat("You did it! You have created paths to the RES, Mills, and CCE Labs shared Data and Functions folders and the project folder on Box.\n", paste("The paths can be called directly using res.data.path, res.func.path, lab.data.path, lab.func.path and proj.path. Go foRth and conqueR!", sep = ""))
