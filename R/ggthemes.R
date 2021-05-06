8f61a87b6140e3fc68a106b436ecad9faf5da07f
Adam <adamkemberling@gmail.com>
2021-05-06 15:32:27 -0400

    un-squished the gmri logo

--- R/ggthemes.R
+++ R/ggthemes.R
@@ -7,7 +7,21 @@
 
 
 
+# set standard publication-figure theme
 
+# to use, add "+ GMRI_standard" to any ggplot object
+
+# GMRI_standard <- theme_light() + theme(panel.background = element_blank(),
+#                                        panel.grid       = element_blank(),
+#                                        panel.border     = element_blank(),
+#                                        axis.line        = element_line(colour = "#333333"),
+#                                        axis.ticks       = element_line(color = "#333333"),
+#                                        axis.text        = element_text(size = 13, color="#333333"),
+#                                        axis.title       = element_text(size = 14, color="#333333"),
+#                                        legend.text      = element_text(size = 13, color="#333333"),
+#                                        legend.title     = element_text(size = 14, color="#333333"),
+#                                        strip.background = element_blank(),
+#                                        strip.text       = element_text(size = 13, color="#333333"))
 
 
 
